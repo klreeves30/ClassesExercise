61cf977af72837a245d6cb6087786f445a54939f
klreeves30 <klreeves30@gmail.com>
2021-11-03 14:35:08 -0400

    initial commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    class Car
+    {
+        public Car()
+        {
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Age = 2016;
+        }
+
+       public string Make { get; set; }
+       public string Model { get; set; }
+        public int Age { get; set; }
+        
+        
+
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car() { Make = "Chevorlet", Model = "Tahoe", Age = 2016 };
+            {
+                Console.WriteLine($"I have a {myCar.Make}\n");
+
+                Console.WriteLine($"The model of it is {myCar.Model}\n");
+
+                Console.WriteLine($"It was made in {myCar.Age}\n");
+            }
+        }
+    }
+}
